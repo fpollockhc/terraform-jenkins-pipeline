8ad5faba6e65eea306cf6552d5b304aebaf5c60f
Fraser Pollock <81329282+fpollockhc@users.noreply.github.com>
2022-02-03 12:36:58 -0500

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,14 +1,7 @@
-data "aws_vpc" "primary-vpc" {
-    default = true
-}
-
 data "aws_ami" "ubuntu" {
     most_recent = true
 
-    filter {
-        name_regex = "name"
-        values = ["ubuntu-minimal/images/hvm-ssd/ubuntu-focal-20*"]
-    }
+    name_regex  = "ubuntu-minimal/images/hvm-ssd/ubuntu-focal-20*"
 
     filter {
         name = "virtualization-type"
